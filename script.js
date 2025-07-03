ca2d10d125e087b3716622b266f99e516f37feeb
geethapriya14 <priyaes090@gmail.com>
2025-07-03 18:11:37 +0530

    all files added

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+  function scrollLeft() {
+    document.getElementById('productScroll').scrollBy({
+      left: -200,
+      behavior: 'smooth'
+    });
+  }
+
+  function scrollRight() {
+    document.getElementById('productScroll').scrollBy({
+      left: 200,
+      behavior: 'smooth'
+    });
+  }
+</script>
